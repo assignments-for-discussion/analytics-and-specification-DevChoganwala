bd86665cbf2c1f0fdf5b3d1294b75e89407808c1
Dev <dev2.choganwala@gmail.com>
2021-12-05 23:00:55 +0530

    Modified average.js to give correct output

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  numbers = numbers.filter(n => !Number.isNaN(n));
+  return numbers.reduce((p, c) => p + c, 0) / numbers.length;
 }
-
-module.exports = {average};
+module.exports = { average };
